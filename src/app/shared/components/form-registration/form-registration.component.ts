5dc291b19e0572d9809ee6986dc0e98a67c805df
Camila Hatakeyama <camila.hatakeyama@dotzmkt.com.br>
2022-06-14 13:33:09 -0300

    feat: adiciona validação básica no formulário de cadastro

--- src/app/shared/components/form-registration/form-registration.component.ts
+++ src/app/shared/components/form-registration/form-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IPokemon } from '../../interfaces/IPokemon.interface';
 
 @Component({
   selector: 'app-form-registration',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormRegistrationComponent implements OnInit {
 
+  pokemonModel = {} as IPokemon
+
+  registrationForm = new FormGroup({
+    nomeControl: new FormControl(this.pokemonModel.nome, [
+      Validators.required, Validators.minLength(4)]),
+    descricaoControl: new FormControl(this.pokemonModel.descricao, Validators.required),
+    alturaControl: new FormControl(this.pokemonModel.altura, Validators.required),
+    pesoControl: new FormControl(this.pokemonModel.peso, Validators.required),
+    categoriaControl: new FormControl(this.pokemonModel.categoria, Validators.required),
+    habilidadeControl: new FormControl(this.pokemonModel.habilidade, Validators.required),
+    ataqueControl: new FormControl(this.pokemonModel.ataque, Validators.required),
+    defesaControl: new FormControl(this.pokemonModel.defesa, Validators.required),
+    velocidadeControl: new FormControl(this.pokemonModel.velocidade, Validators.required),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get nome() { return this.registrationForm.get('nomeControl'); }
+  get descricao() { return this.registrationForm.get('descricaoControl'); }
+  get altura() { return this.registrationForm.get('alturaControl'); }
+  get peso() { return this.registrationForm.get('pesoControl'); }
+  get categoria() { return this.registrationForm.get('categoriaControl'); }
+  get habilidade() { return this.registrationForm.get('habilidadeControl'); }
+  get ataque() { return this.registrationForm.get('ataqueControl'); }
+  get defesa() { return this.registrationForm.get('defesaControl'); }
+  get velocidade() { return this.registrationForm.get('velocidadeControl'); }
+
+  onSubmit = () => {
+    console.log(this.registrationForm.value)
   }
 
 }
---
+++ src/app/shared/interfaces/IPokemon.interface.ts
@@ -0,0 +1,12 @@
+export interface IPokemon {
+    id: number
+    nome: string
+    descricao: string
+    altura: string
+    peso: number
+    categoria: string
+    habilidade: string
+    ataque: string
+    defesa: string
+    velocidade: string
+}
