3741bcf14843cdcff3b99b687f712eca05032be4
Camila Hatakeyama <camila.hatakeyama@dotzmkt.com.br>
2022-06-15 01:35:19 -0300

    feat: cria navbar inferior para mobile

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-  
+
   events: string[] = []
   opened: boolean = true
+  ADD_LABEL = 'Adicionar'
+  LIST_LABEL = 'Lista'
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigateTo = (event: any): void => {
+    if (event.tab.textLabel === this.ADD_LABEL) {
+      this.router.navigate(['/add'])
+    } else if (event.tab.textLabel === this.LIST_LABEL) {
+      this.router.navigate(['/list'])
+    }
+  }
+
+  getCurrentRouteIndex = (): number => {
+    const routes = ['/add', '/list']
+    return routes.findIndex(r => r === this.router.url)
+  }
 }
